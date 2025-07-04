3d90703664c5a21ff9a1d6cfce1f257ada2f64c0
Divii018 <divyaprabu2004@gmail.com>
2025-07-04 14:35:21 +0530

    my project changes

---
+++ JS/intro.js
@@ -0,0 +1,101 @@
+// console.log("divyaa");
+//var c;
+//let a,b;
+//a=1;
+//b=2;
+//c=a+b;
+//console.log(c)
+//console.log("Addition of two nos:" a+b)
+//console.log(a+b)
+//console.log(`Adiition of two nos: ${a+b`})
+//var a="One";
+//a=1;
+//console.log(a++);
+//console.log(a);
+//console.log(++a);
+//var b=null;
+//console.log(typeof b);
+
+
+//declarative function
+/*
+function add(){
+    let age=18; 
+    if(age>=18){
+        console.log("Eligible to VOte");
+        
+    }
+    else{
+        console.log("Not eligible to vote")
+    }
+}
+add();
+
+
+//parametric function
+function addd(a,b){
+    console.log(a+b);
+}
+addd(10,20);
+
+let trafficlight="Red";
+if(trafficlight=="Red"){
+    console.log("STOP");
+    
+}
+
+if(trafficlight=="Yellow"){
+    console.log("Ready");
+    
+}
+
+if(trafficlight=="Green"){
+    console.log("Go");
+    
+}
+
+for(let i=0;i<=5;i++){
+    console.log(i);
+    
+}
+
+var a="5";
+var b="5";
+if(a==b){
+    console.log("Both are equal");
+    
+}
+
+if(a===b){
+    console.log("Both type and value are equal");
+    
+}
+*/
+/*
+var arr=["Saab", "Volvo", "BMW"];
+console.log(arr);
+var a=arr[0];
+console.log(a);
+console.log(arr.length);
+arr[3]="maruthi";
+console.log(arr);
+arr.push(100);   //add element at end
+console.log(arr);
+arr.unshift("tata"); //add element at beginning
+console.log(arr);
+arr[0]="end"        //elemnt modified
+console.log(arr); 
+arr.pop();
+console.log(arr); //pop used last element is removed
+arr.shift();
+console.log(arr); //shift used element at start is removed  
+*/
+
+var person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+  things: {Name1:"abc",class:"c"},
+};
+console.log(person);
